ea3f523c0e4acd2afc83a36338d5b5c6388dadc8
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2025-04-01 00:01:20 -0400

    Time: 3 ms (34.68%) | Memory: 12.5 MB (27.78%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-            print("")
-        else:
-            prefix = strs[0]
-            for s in strs[1:]:
-                while not s.startswith(prefix):
-                    prefix = prefix[:-1]
-                    if not prefix:
-                        break
-        return prefix
+        n = len(strs)
+        print(strs[0])
+        a = strs[0]
+        l = len(strs[0])
+        for i in range(n):
+            while not strs[i].startswith(a):
+                a = a[:-1]
+                if not a:
+                    break
+        return a
