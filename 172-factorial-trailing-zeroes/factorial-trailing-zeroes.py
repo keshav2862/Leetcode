dda8b55e92734615ae29e2da51a9a5e9efeb61c2
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2025-08-19 15:53:37 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.37%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 0
-        else:
-            fact = 1
-            for i in range(1, n + 1):
-                fact *= i
-            return len(str(fact)) - len(str(fact).rstrip('0'))
+        count = 0
+        while n >= 5:
+            n //= 5
+            count += n
+        return count
