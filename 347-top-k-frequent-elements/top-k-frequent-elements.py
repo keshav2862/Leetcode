ecc4c219a62e814d0546043a8e47432f8ff17fe1
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-02-09 11:13:52 -0800

    Time: 15 ms (38.31%) | Memory: 14.4 MB (61.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -5,8 +6,6 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        freq = {}
-        for i in nums:
-            freq[i] = freq.get(i,0)+1
-        ans = sorted(freq.items(), key = lambda x:x[1],reverse = True)
-        return [key for key,val in ans[:k]]
+        freq = Counter(nums)
+        ans = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+        return [key for key, val in ans[:k]]
