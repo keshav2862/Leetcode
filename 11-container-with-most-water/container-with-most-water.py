884f6ba26ad28da330af8d816de07435cc567e85
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-02-10 15:08:25 -0800

    Time: 119 ms (52.72%) | Memory: 20.8 MB (21.04%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -5,17 +5,17 @@ class Solution(object):
         :rtype: int
         """
         n = len(height)
-        start = 0
-        end = n-1
+        s= 0
+        e = n-1
         i = 0
-        area = []
-        while i < n:
-            h = min(height[start],height[end])
-            l = end - start
-            area.append(l*h)
-            if height[start] > height[end]:
-                end = end -1
+        area = 0
+        while s < e:
+            h = min(height[s],height[e])
+            l = e - s
+            area = max(area,l*h)
+            if height[s] < height[e]:
+                s = s+1
             else:
-                start = start + 1
-            i = i+1
-        return max(area)
+                e = e-1
+        return area
+                
