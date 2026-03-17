e5bf32202620f5f9f8196cdb1425cfe8dbc7e7ac
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-03-17 15:37:12 -0700

    Time: 229 ms (92.45%) | Memory: 78.5 MB (43.93%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,8 +2,8 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
 
 class LRUCache(object):
 
