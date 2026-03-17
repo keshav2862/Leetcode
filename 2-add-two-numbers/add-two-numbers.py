fc3c5c5af4327b2c22a82f49330c1f7fd7908c2c
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-03-17 15:13:59 -0700

    Time: 8 ms (52.79%) | Memory: 12.6 MB (49.11%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -11,15 +11,17 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         carry = 0
-        root = n = ListNode(0)
+        dummy = ListNode(0)
+        curr = dummy
         while l1 or l2 or carry:
+            val = carry
             if l1:
-                carry = carry + l1.val
+                val = val + l1.val
                 l1 = l1.next
             if l2:
-                carry = carry + l2.val
+                val = val+l2.val
                 l2 = l2.next
-            carry, val = divmod(carry,10)
-            n.next = ListNode(val)
-            n = n.next
-        return root.next
+            carry,val = divmod(val,10)
+            curr.next = ListNode(val)
+            curr = curr.next
+        return dummy.next
