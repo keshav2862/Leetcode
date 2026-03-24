f36df8a5f84c2fa96151a6df8b7d4484f50631c1
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-03-24 15:46:24 -0700

    Time: 7 ms (56.30%) | Memory: 19.9 MB (76.43%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -38,42 +38,45 @@ class MyCircularQueue:
     #         return True
     #     return False
     def __init__(self, k: int):
-        self.queue = [0] * k
-        self.head = 0
-        self.tail = 0
-        self.size = 0
-        self.capacity = k
+        self.k=k
+        self.q=[None]*k
+        self.rear=-1
+        self.front=0
+        self.size=0
+        
 
     def enQueue(self, value: int) -> bool:
-        if self.isFull():
+        if(self.isFull()):
             return False
-        self.queue[self.tail] = value
-        self.tail = (self.tail + 1) % self.capacity
-        self.size += 1
+        self.rear=((self.rear+1)%self.k)
+        self.q[self.rear]=value
+        self.size+=1
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
-        self.head = (self.head + 1) % self.capacity
-        self.size -= 1
+        self.q[self.front]=None
+        self.front=((self.front+1) % self.k)
+        self.size-=1
         return True
 
     def Front(self) -> int:
         if self.isEmpty():
-            return -1
-        return self.queue[self.head]
+            return -1  
+        return self.q[self.front]     
 
     def Rear(self) -> int:
         if self.isEmpty():
-            return -1
-        return self.queue[(self.tail - 1 + self.capacity) % self.capacity]
+            return -1 
+        return self.q[self.rear]       
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self.size==0
 
     def isFull(self) -> bool:
-        return self.size == self.capacity
+        return self.size==self.k
+
 
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
