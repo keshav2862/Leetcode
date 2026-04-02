c7d50d8345262e2a3a38a8de4588e56fcbd7fd17
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-04-02 15:09:03 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.07%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         c = nums[0]
         pc = 0
         i = 1
-        li = 0
         while i < len(nums):
-            t = c
-            c = max(c,pc + nums[i])
-            pc = t
-            i = i + 1
+            temp = c
+            c = max(pc + nums[i],c)
+            pc = temp
+            i = i+1
         return c
