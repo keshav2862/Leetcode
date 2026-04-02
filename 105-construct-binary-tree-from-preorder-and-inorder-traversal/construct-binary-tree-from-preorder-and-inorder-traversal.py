3740645462d6c98f411c0945dd64cbab70c2c3ee
Keshav Narayan Srinivasan <128219496+keshav2862@users.noreply.github.com>
2026-04-02 11:40:21 -0700

    Time: 0 ms (100.00%) | Memory: 21 MB (72.27%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,28 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: Optional[TreeNode]
-        """
-        self.preroot = 0
-        hmap = {val: x for x,val in enumerate(inorder)}
-        def finder(left,right):
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        hmap = {val:i for i,val in enumerate(inorder)}
+        self.preindex = 0
+        def build(left,right):
             if left>right:
                 return None
-            k = preorder[self.preroot]
-            self.preroot += 1
-            root = TreeNode(k)
-            idx = hmap[k]
-            root.left = finder(left,idx -1)
-            root.right = finder(idx+1,right)
+            rootval = preorder[self.preindex]
+            self.preindex +=1
+            root = TreeNode(rootval)
+            mid = hmap[rootval]
+            root.left = build(left,mid-1)
+            root.right = build(mid+1,right)
             return root
-        return finder(0, len(inorder)-1)
-
-        
+        return build(0,len(preorder)-1)
